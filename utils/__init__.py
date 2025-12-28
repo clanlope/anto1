9ed5ecb90adfea165c416f8d6f45b70fbe4c68b7
Clanlope <32638534+clanlope@users.noreply.github.com>
2025-12-28 18:33:48 +0800

    Initial commit

---
+++ utils/__init__.py
@@ -0,0 +1,1 @@
+from .files_utils import *
---
+++ utils/files_utils.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+import inspect
+
+
+def current_dir() -> Path:
+    """Returns the directory of the current file."""
+    return Path(inspect.stack()[-1].filename).resolve().parent
+
+
+def ensure_dir(path: Path) -> Path:
+    """Ensure directory exists. Create it if missing."""
+    return Path(path).mkdir(parents=True, exist_ok=True)
+
+
+def list_files(suffix: str = "", directory: Path = None) -> list[Path] | list[None]:
+    """Lists all files in the given directory."""
+    suffix = suffix.lower() if suffix.startswith(".") else f".{suffix.lower()}"
+    directory = current_dir() if directory is None else directory
+    return [
+        _
+        for _ in directory.iterdir()
+        if _.is_file() and (suffix == "" or _.suffix == suffix)
+    ]
+
+
+def select_file(list: list[Path]) -> Path | None:
+    """⚪ Selects a file from the file list: [0 quit]"""
+    print(inspect.getdoc(select_file))
+    [print(i + 1, v) for i, v in enumerate(list)]
+    while True:
+        try:
+            res = list[u - 1] if (u := int(input())) else None
+            print(f"🔵 Your selection: {res.name if res else 'None'}")
+            return res
+        except Exception as e:
+            print(f"❌ Invalid selection, try again. -> {e}")
