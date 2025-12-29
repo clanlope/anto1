656a08927a95e2c4b2cf78a8b80cb9379b70761f
Clanlope <32638534+clanlope@users.noreply.github.com>
2025-12-29 22:46:02 +0800

    timer
    1

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,17 @@
 from .files_utils import *
 from .coms_utils import *
+from .webs_utils import *
+
+
+def timer(func):
+    import time, functools
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f"🕒 Execution completed in {end - start:.1f} seconds.")
+        return result
+
+    return wrapper
